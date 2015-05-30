1e2614bae5040454aa619ff561d7c7e18e96c989
wara <kusabashira227@gmail.com>
2015-05-30 22:20:24 +0900

    Implement wait continuous modify

--- monico.go
+++ monico.go
@@ -29,12 +29,37 @@ func NewMoniterWithWD() (*Moniter, error) {
 	return NewMoniter(wd)
 }
 
+func (m *Moniter) waitContinuousModify() error {
+	lastModTime := m.lastModTime
+	for {
+		time.Sleep(100 * time.Millisecond)
+		info, err := os.Stat(m.path)
+		if err != nil {
+			return err
+		}
+
+		newModTime := info.ModTime()
+		if lastModTime.Equal(newModTime) {
+			break
+		}
+		lastModTime = newModTime
+	}
+	return nil
+}
+
 func (m *Moniter) Modified() (bool, error) {
 	info, err := os.Stat(m.path)
 	if err != nil {
 		return false, err
 	}
-	return !m.lastModTime.Equal(info.ModTime()), nil
+	modified := !m.lastModTime.Equal(info.ModTime())
+	if !modified {
+		return false, nil
+	}
+	if err = m.waitContinuousModify(); err != nil {
+		return false, err
+	}
+	return true, nil
 }
 
 func (m *Moniter) UpdateModTime() error {
