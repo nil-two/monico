fe56801b496dbf798bbcc0268f3b69aa8a21355d
wara <kusabashira227@gmail.com>
2015-05-30 13:08:32 +0900

    Add test for Path

--- monico_test.go
+++ monico_test.go
@@ -1,11 +1,36 @@
 package monico
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
+var tempDir string
+
 func TestMain(m *testing.M) {
+	path, err := ioutil.TempDir("", "")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "monico_test:", err)
+	}
+	tempDir = path
+
 	e := m.Run()
 	defer os.Exit(e)
+
+	os.RemoveAll(tempDir)
+}
+
+func TestPath(t *testing.T) {
+	m, err := NewMoniter(tempDir)
+	if err != nil {
+		t.Errorf("NewMoniter returns %q, want nil", err)
+	}
+	expect := tempDir
+	actual := m.Path()
+	if actual != expect {
+		t.Errorf("got %q, want %q",
+			actual, expect)
+	}
 }
