5435d545f39e1b8c0b256a2ab7e029119ee6d7a6
wara <kusabashira227@gmail.com>
2015-05-30 17:46:30 +0900

    Implement usage

--- cmd/monico/monico.go
+++ cmd/monico/monico.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"os"
+)
+
+func usage() {
+	os.Stderr.WriteString(`
+Usage: monico [OPTION]... COMMAND [ARGS]...
+Run COMMAND if there is modified current directory.
+
+Options:
+	-d, --dir=PATH   cd PATH before monitering
+	-h, --help       show this help message
+`[1:])
+}
+
 func main() {
 }
